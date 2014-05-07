4eebd709a9f12117abc0f2f7d494e40546dc7383
Pavle <paavlee@yahoo.com>
2014-05-07 21:50:20 +0200

    Update day2.js

--- day2.js
+++ day2.js
@@ -2,17 +2,17 @@
  * Created by JS on 5/7/14.
  */
 
-
+// Create Calculator object with average method
 var Calculator = {
     average: function() {
-        var sum = 0;
-        if (arguments.length === 0){
+        var sum = 0; 
+        if (arguments.length === 0){ // If there 0 arguments passed return 0
             return 0;
         }
-        for (var i = 0; i < arguments.length; i++){
+        for (var i = 0; i < arguments.length; i++){ // Calculate numbers and add them to sum value
             sum += arguments[i];
         }
-        return sum / arguments.length;
+        return sum / arguments.length; // Divide sum with number of arguments passed
     }
 };
 
