c354eafee76ab8256a47cdd2f5a2eb65e285d436
Pavle <paavlee@yahoo.com>
2014-05-08 21:20:59 +0200

    Update day3.js

--- day3.js
+++ day3.js
@@ -1,13 +1,17 @@
 /**
  * Created by JS on 5/8/14.
  */
-
+// Self Invoked Function
 (function(){
+    // Create Work Object 
     var work = {
+        // Properties
         fizz : "Fizz",
         buzz : "Buzz",
         fizzbuzz : "FizzBuzz",
+        // Method
         solution : function(){
+            // Go through all numbers from 1 to 100 and if is it divisable by both 3 and 5 , console.log fizzbuzz propertie, etc.
             for (var i = 1; i <= 100; i++) {
                 if ((i % 3 === 0) && (i % 5 === 0)){
                     console.log(this.fizzbuzz);
@@ -21,5 +25,7 @@
             }
         }
     };
+    // Call method
     work.solution();
 })();
+//Call self invoking function
