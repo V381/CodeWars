cd696b4eaebb4fa9ab7027f884589e3604eb4982
Pavle <paavlee@yahoo.com>
2014-05-13 22:56:27 +0200

    Update day6.js

--- day6.js
+++ day6.js
@@ -2,6 +2,10 @@
  * Created by JS on 5/13/14.
  */
 
+// Object.keys Method = returns an array of a given object's own enumerable properties
+// Map Method = Add value to every element of array(callback, index, array)
+// Join Method = Add , between elements or any other string
+
 function solution(pairs){
     return Object.keys(pairs).map(function(e){return e +" = " + pairs[e]}).join(",");
 }
