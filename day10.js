9aef06cd847196471c54d51ab1e4f3a157f44efa
Pavle <paavlee@yahoo.com>
2014-05-25 22:44:10 +0200

    day 10
    Shifty Closures

---
+++ day10.js
@@ -0,0 +1,16 @@
+/**
+ * Created by JS on 5/24/14.
+ */
+
+
+var greet_abe = function() {
+    var name = 'Abe';
+    return "Hello, " + name + '!';
+};
+var greet_ben = function() {
+    var name = 'Ben';
+    return "Hello, " + name + '!';
+};
+
+greet_abe();
+greet_abe();
