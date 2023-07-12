42c6e64500862c04d265f44b74ee0a6f2ba480a1
Pavle Paunovic <paavlee@yahoo.com>
2023-07-12 12:35:42 +0200

    New kata

---
+++ 2023/7 Kyu/How many are smaller than me.js
@@ -0,0 +1,17 @@
+function smaller(nums) {
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] < nums[i]) {
+                count++;
+            }
+        }
+
+        result.push(count);
+    }
+    console.log(result);
+    return result;
+}
--- 2023/Kyu 6/playground.js
+++ 2023/Kyu 6/playground.js
@@ -400,3 +400,31 @@ const nikita = [
           }
           return res;
       }
+
+
+function smaller(nums) {
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] < nums[i]) {
+                count++;
+            }
+        }
+
+        result.push(count);
+    }
+    console.log(result);
+    return result;
+}
+
+smaller([5,4,3,2,1]);
+// smaller([1,2,3])
+// smaller([1,2, 0]);
+// smaller([1,2,1]);
+// smaller([1, 1, -1, 0, 0])
+// Better: Write a function that given, an array arr, returns an array containing 
+// at each index i the amount of numbers arr[j] that are smaller than arr[i] for j>i.
+// assert.deepEqual(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0]);
